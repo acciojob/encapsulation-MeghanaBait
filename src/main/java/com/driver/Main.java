e55425d2e4b23e21769c2b9edf2206657b5c6595
Meghana Bait <meghanawork777@gmail.com>
2023-08-10 20:40:53 +0530

    Adding Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj1 = new RWOnly();
+        obj1.setName("Meghana");
+        String name = obj1.getName();
+
+    }
   
 }
